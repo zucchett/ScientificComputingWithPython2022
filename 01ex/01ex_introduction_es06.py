e93224c86fc65487ff28a56fd70f6142b8ca400a
maresoo <leo.mareso@studenti.unipd.it>
2022-10-15 13:38:58 +0200

    Exercises of the first week

---
+++ 01ex/01ex_introduction_es06.py
@@ -0,0 +1,28 @@
+# Casting
+
+x = input("Insert first element: ")
+y = input("Insert second element: ")
+
+try:
+	x = int(x)
+except:
+	try:
+		x = float(x)
+	except:
+		pass
+	
+try:		
+	y = int(y)
+except:
+	try:
+		y = float(y)
+	except:
+		pass
+
+print("type(x) = " + str(type(x)) + ", type(y) = " + str(type(y)))
+
+try:
+	z = x + y
+	print("Operation successful: result is \"" + str(z) + "\".")
+except:
+	print("Operation failed.")
