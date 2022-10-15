3e05d4ab71d9d33aff06106a35fcd59fdac8c7d1
maresoo <leo.mareso@studenti.unipd.it>
2022-10-15 13:34:59 +0200

    Deleting directory

--- 01ex/01ex_introduction_es02.py
+++
@@ -1,8 +0,0 @@
-# The swap
-
-x = input("Write the first variable: ")
-y = input("Write the second variable: ")
-
-x,y = y,x
-print("x = " + str(x) + ", y = " + str(y))
-
--- 01ex/01ex_introduction_es08.py
+++
@@ -1,3 +0,0 @@
-# List comprehension
-
-print([(i,j) for i in range(3) for j in range(4)])
