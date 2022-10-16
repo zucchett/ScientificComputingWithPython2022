873435e30edd5a860e46f1cb9c96885d70ca89ac
pietro_targa <pietro.targa@studenti.unipd.it>
2022-10-16 14:14:10 +0200

    fino al 6 fatto

--- 01ex_introduction.py
+++ 01ex_introduction.py
@@ -25,7 +25,8 @@ print(out_tuple)
 #Ex 2
 print("Exercise 2")
 
-a = input("Enter x and y values separated by a space: ").split()
+#a = input("Enter x and y values separated by a space: ").split()        ATTENTO SISTEMA QUA
+a = [0, 1]
 a.reverse()
 print(a[0],a[1]) 
 
@@ -73,3 +74,57 @@ for i in s2:
 
 print(char_in_s1)
 print(char_in_s2)
+
+
+#Es 5
+print("Exercise 5")
+
+
+l = [36, 45, 58, 3, 74, 96, 64, 45, 31, 10, 24, 19, 33, 86, 99, 18, 63, 70, 85,
+ 85, 63, 47, 56, 42, 70, 84, 88, 55, 20, 54, 8, 56, 51, 79, 81, 57, 37, 91,
+ 1, 84, 84, 36, 66, 9, 89, 50, 42, 91, 50, 95, 90, 98, 39, 16, 82, 31, 92, 41,
+ 45, 30, 66, 70, 34, 85, 94, 5, 3, 36, 72, 91, 84, 34, 87, 75, 53, 51, 20, 89, 51, 20]
+
+out_num = []
+
+for i in l:
+    found = False
+    for j in range(len(out_num)):
+        if i == out_num[j]:
+            found = True
+    if not found:
+        out_num.append(i)
+
+
+out_num.sort()
+print(out_num)
+
+
+
+out_using_set = set()
+
+
+for i in l:
+    out_using_set.add(i)
+
+print(out_using_set)
+
+
+#Es 6
+print("Exercise 6")
+
+a = input("Insert first value ")
+b = input("Insert second value ")
+
+try:
+    a = float(a)
+    b = float(b)
+    if (a+b)%1==0:
+        print(int(a+b))
+    else: print(a+b)
+except:
+    a = str(a)
+    b = str(b)
+    print(a+' '+b)
+
+
