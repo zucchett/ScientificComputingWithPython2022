4aed0e41b70485d65998fcf3353c1317feed804e
sreeshalinimada <sreeshalini809@gmail.com>
2022-10-16 21:25:42 +0200

    HW_1 finished

---
+++ HW_1/01ex_introduction_02es.py
@@ -0,0 +1,14 @@
+
+# input
+print("enter the value of x")
+x = input()
+print("enter the value of y")
+y = input()
+
+print(x +" is the original value of x")
+print(y +" is the original value of y")
+
+x,y=y,x
+
+print(x +" is the new value of x")
+print(y +" is the new value of y")
