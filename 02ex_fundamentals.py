48d680296fac693cfd5c70b6022a246d2d1da54e
RiccardoMosele <riccardo.mosele@studenti.unipd.it>
2022-10-21 15:53:51 +0200

    es2 conclusi ma da sistemare

--- 02ex_fundamentals.py
+++ 02ex_fundamentals.py
@@ -16,7 +16,7 @@ passive = False
 
 printEs1=passive  #esercizio 2.1
 printEs2=passive  #esercizio 2.2
-printEs3=passive  #esercizio 2.3
+printEs3=active  #esercizio 2.3
 printEs4=passive  #esercizio 2.4
 printEs5=passive  #esercizio 2.5
 printEs6=passive  #esercizio 2.6
@@ -59,9 +59,8 @@ if(printEs2):
 if(printEs3):
     print("\n\n***************************************************Ex 2.3")
 
-
     listWord = ["Ciao", "Buongiorno", "Buonasera", "Buonanotte", "Salve"]
-    
+
     try:
         x = int(input("Insert the max lenght: "))
         res=[]
@@ -109,22 +108,203 @@ if(printEs6):
 if(printEs7):
     print("\n\n***************************************************Ex 2.7")
 
+    def hello(func):
+        def wrapper(x):
+            print("Hello World")
+            return func(x)
+        return wrapper
+
+    @hello
+    def square(x):
+        return x*x
+
+    x=square(2)
+    print(f"Res function: {x}")
+
 
 #***************************************************Ex 2.8
 if(printEs8):
     print("\n\n***************************************************Ex 2.8")
 
+    def fib(n):
+        if n == 0:
+            return 0
+        if n == 1:
+            return 1
+        else:
+            return fib(n-1) + fib(n-2)
+
+    fibSeq=[]
+    for i in range(20):
+        fibSeq.append(fib(i))
+
+    print(f"First {len(fibSeq)} numbers of Fibonacci sequence:\n{fibSeq}")
+    
 
 #***************************************************Ex 2.9
+import timeit
 if(printEs9):
     print("\n\n***************************************************Ex 2.9")
+
+    def loopFibonacci(m):
+        fibSeq=[1,1]
+        for i in range(m):
+            fibSeq.append(fibSeq[len(fibSeq)-1]+fibSeq[len(fibSeq)-2])
+        return fibSeq
+
+    def recurviveFibonacci(m):
+        def fib(n):
+            if n == 0:
+                return 0
+            if n == 1:
+                return 1
+            else:
+                return fib(n-1) + fib(n-2)
+
+        fibSeq=[]
+        for i in range(m):
+            fibSeq.append(fib(i))
+        return fibSeq
+
+    resLoop = timeit.timeit(stmt='loopFibonacci(20)', globals=globals(), number=1)
+    resRec = timeit.timeit(stmt='recurviveFibonacci(20)', globals=globals(), number=1)
+
+    print(f"With LOOP: {resLoop}")
+    print(f"With RECURSIVE{resRec}")
+
 
 
 #***************************************************Ex 2.10
 if(printEs10):
     print("\n\n***************************************************Ex 2.10")
 
+    class polygon:
+        pol=[]
 
+        def __init__(self, data):
+            if(len(data)<3):
+                print("Insert at least 3 element!")
+                self.__del__()
+            self.pol=list(data)
+
+        def __del__(self):
+            print("Goodbye")
+
+        def getSide(self, pos):
+            pos=pos-1
+            if(pos<0):
+                print("ERROR: wrong value")
+                return 0
+            else:
+                return self.pol[pos]
+
+        def setSide(self, pos, value):
+            pos=pos-1
+            if(pos<0):
+                print("ERROR: wrong value")
+            else:
+                self.pol[pos]=value
+
+        def perimeter(self):
+            per=0
+            for i in self.pol:
+                per=per+i
+                print(per)
+            return per
+
+        def getOrderedSides(self, increasing):
+                return tuple(sorted(self.pol,reverse=not increasing))
+
+        def printAll(self):
+            print(self.pol) 
+
+      
+    x = polygon((40,10,20,50,7))
+    print("Initial polygon: ")
+    x.printAll()
+
+    print("\nInsert the new value 30 instead 7 (last side)")
+    x.setSide(5,30)
+
+    print("New polygon: ")
+    x.printAll()
+
+    print(f"\nPerimeter: {x.perimeter()}")
+
+    print("\nSort in increasing and decreasing order: ")
+    tmp = x.getOrderedSides(increasing = True)
+    print(tmp)
+
+    tmp = x.getOrderedSides(increasing = False)
+    print(tmp)
+
+    print("\nPolygon:")
+    x.printAll()
+
+    
+    
 #***************************************************Ex 2.11
 if(printEs11):
     print("\n\n***************************************************Ex 2.11")
+
+    class polygon:
+        pol=[]
+
+        def __init__(self, data):
+            if(len(data)<3):
+                print("Insert at least 3 element!")
+                self.__del__()
+            self.pol=list(data)
+
+        def __del__(self):
+            print("Goodbye")
+
+        def getSide(self, pos):
+            pos=pos-1
+            if(pos<0):
+                print("ERROR: wrong value")
+                return 0
+            else:
+                return self.pol[pos]
+
+        def setSide(self, pos, value):
+            pos=pos-1
+            if(pos<0):
+                print("ERROR: wrong value")
+            else:
+                self.pol[pos]=value
+
+        def perimeter(self):
+            per=0
+            for i in self.pol:
+                per=per+i
+                print(per)
+            return per
+
+        def getOrderedSides(self, increasing):
+                return tuple(sorted(self.pol,reverse=not increasing))
+
+        def printAll(self):
+            print(self.pol) 
+
+    class rectangle(polygon):
+
+        def __init__(self, data):
+            if(len(data)==2):
+                self.pol=list(data)
+                self.pol.append(data[0])
+                self.pol.append(data[1])
+            else:
+                print("Insert 2 element: base and height")
+                self.__del__()
+
+        def area(self):
+            return self.pol[0]*self.pol[1]
+
+
+    myRect = rectangle((10,20))
+
+    print("Print all side of rect: ")
+    myRect.printAll()
+
+    print(f"\nArea:{myRect.area()}")
