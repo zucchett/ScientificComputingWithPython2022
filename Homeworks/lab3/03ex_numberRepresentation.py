0b637d4ce4362872c2d43e9f439d35c3f4c7511c
Gianpietro Nicoletti <gianpietro.nicoletti@studenti.unipd.it>
2022-10-30 11:58:04 +0100

    Completed 03ex

--- Homeworks/lab3/03ex_numberRepresentation.py
+++ Homeworks/lab3/03ex_numberRepresentation.py
@@ -1,4 +1,5 @@
 import math as mt
+import timeit
 
 print("#####################EX1#####################")
 #ex1
@@ -144,14 +145,15 @@ i=1
 while(True):#adding ad each iteration a factor 10^-i
 	
 	new_number = initial_number + 10**(-1*i)
+	print(10**(-1*i))
 	print(new_number)
-	if(new_number == initial_number): #if the initial number doesn't change -> reach the limit 
+	if(new_number == 1): #if the initial number doesn't change -> reach the limit 
 					  # of the precision 
 		break
 	
 	i = i + 1
 
-print("the precision of the machine is: ", 10**(-1*(i-1))) #with the final value of the variable i
+print("The precision of the machine is: ", 10**(-1*(i-1))) #with the final value of the variable i
 							   #there was no effect in the number so 
 							   #we need to take the previous one ->i-1
 
@@ -248,7 +250,8 @@ for solution in [(1,0,-1),(9,-6,1),(1,1,12), (0.001,1000,0.001),(0.001, -1000, 0
 		print("The solution of the equation are x1 = x2 = ", solved[0])
 	
 
-#the difference can be caused by the fact that we are subtracting two numbers very close (b and sqrt(delta)
+#ANSWER:
+#the difference can be caused by the fact that we are subtracting two numbers very close (b and sqrt(delta).
 
 #it's possible to find one of the solution using the standard formula (with the sign that avoid to perform the cancellation) and then using the fact that x1*x2 = c/a
 
@@ -305,6 +308,117 @@ for solution in [(1,0,-1),(9,-6,1),(1,1,12), (0.001,1000,0.001), (0.001, -1000, 
 	elif len(solved) == 1: 
 		
 		print("The solution of the equation are x1 = x2 = ", solved[0])
+
+print("#####################EX6####################")
+#ex6
+
+def f(x): #return the value of the function for an input x
+	
+	return (x*(x-1)) 
+	
+x = 1
+
+for i in [-2,-4,-6,-8,-10,-12,-14]: #exponent of the delta coefficent
+
+	der = (f(x+10**i) - f(x))/(10**i) #compute the derivate
+	
+	print("The result with delta = ", 10**i," is: ", der) 
+
+#The derivative is 2x-1 = 1 for x = 1
+#The error is caused by the fact that 10^-i is not perfectly equal to (1/10)^-i
+
+print("#####################EX7####################")
+#ex7
+
+def semicircleIntegral(N):
+
+	if(N == 0): #check if N is greater than 0
+		print("N must be >= 0!")
+		return 
+		
+		
+	delta = 2/N 
+	x = -1
+	integral = 0
+	
+	for i in range(N): 
+	
+		integral = integral + delta*mt.sqrt(1-x**2)
+		x = x + delta #new value of x
+	
+	return integral
+	
+
+print("The integral computed with N = 100 is equal to: ",semicircleIntegral(100))
+
+
+#Set condition = True if you want run the following peace of code:
+
+condition = False
+
+time = 0
+N=1
+while(condition): #until i not reach the exit condition
+	
+	start_time = timeit.default_timer() #initial time
+	result = semicircleIntegral(N) #compute the integral
+	end_time = timeit.default_timer() #end time
+	
+	time = end_time - start_time #execution time
+	
+	# print("Time with N = ", N, " is : ",time)
+	
+	if(time >= 1): #exit condition
+	
+		 break
+
+	#try to improve the accuracy of the estimation
+	if(time <= 0.5): 
+		 
+		N = N*2
+		
+		
+	#when i nearer 1 second -> reduce the increasing of delta at each iteration
+	elif(time<=0.8): 
+	
+		N = int(N*1.5)
+		
+		
+	elif(time<=0.9):
+	
+		N=int(N*1.1)
+		
+		
+	else:
+		N = N + 1 
+
+if(condition): #print only if i run the previous code
+	
+	print("\n\nI can use ",N - 1," as maximum value of N that allow me to not compute the integral in over than one second")
+	
+
+#A consideration must be done: since the underflow of the float numbers is more or less 10^-15 /
+#10^-16 and i computed delta as 2/N, at a certain value of N, delta becomes 0, and this means that we
+#are not increasing x anymore since we are using x = x + delta at each iteration. So the value of x
+#stucks at a fixed value = -1.
+
+#Infact if i used N = 10000000000000000 i obtain:
+
+
+print("\n\nThe value of x at each iteration with N >= 10000000000000000 is: ", -1 + 2*10**(-17))
+
+#So, find a value for N that runs the code for 1 minutes is usless and it will used to compute a not correct value for the integral.
+
+
+
+
+
+
+
+
+
+
+
 
 
 
