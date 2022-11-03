493af587ce8cd2cc94aa018b5472724210c85938
Fatemeh-1234 <fatemeh.taherzadeh@studenti.unipd.it>
2022-11-03 11:35:39 +0100

    exe

---
+++ 01ex_introduction_es03.py
@@ -0,0 +1,6 @@
+x1= (3,0)
+y1= (0,4)
+
+import math
+d= math.sqrt((x1[1]-x1[0])**2+(y1[1]-y1[0])**2)
+print(d)
---
+++ 01ex_introduction_es06.py
@@ -0,0 +1,13 @@
+
+try:
+    x=int(input())
+    y=int(input())
+    print(x+y)
+except:
+    print("Show error")
+
+
+
+
+
+2
