8105e2fbee49818485b96c701d2196c2857c987d
amirhossein.kargarkhabbazisardroud <amirhossein.kargarkhabbazisardroud@studenti.unipd.it>
2022-11-03 21:25:01 +0100

    ex03

--- 03ex_numberRepresentation_es05.py
+++ 03ex_numberRepresentation_es05.py
@@ -1,29 +1,30 @@
 import math
-print("you can also enter a=0.001, b=1000 and c=0.001")
+print("suggest nimbers: a=0.001, b=1000 and c=0.001")
+def cal(a , b , D):
+    x1 = ((b* -1) - math.sqrt(D)) /( 2 * a)
+    x2 = ((b* -1) + math.sqrt(D)) /(2 * a)  
+    return x1 , x2
 a = float(input("enter a: "))
 b = float(input("enter b: "))
 c = float(input("enter c: "))
 
-#formula
-delta = (b**2) - (4*a*c)
-print(delta)
-x1 = ((b* -1) - math.sqrt(delta)) /( 2 * a)
-x2 = ((b* -1) + math.sqrt(delta)) /(2 * a)
-print("(a)")
+D = (b**2) - (4*a*c)
+x1 , x2 = cal(a , b , D)
+print("(a): ")
 print(x1)
 print(x2)
 
-print("(b)")
-x11 = ((b* -1) - math.sqrt(delta)) 
-x22 = ((b* -1) + math.sqrt(delta)) 
-print(x11)
-print(x22)
-# in compaire with last answer, this time as there is no /2a, so the numbers got bigger
-print("c")
+print("(b): ")
+X1 = ((b* -1) - math.sqrt(D)) 
+X2 = ((b* -1) + math.sqrt(D)) 
+print(X1)
+print(X2)
+# as we remove the mission of /2a, so the answer got bigger
+print("(c): ")
 def findRoot(a,b,c):
-    delta = (b**2) - (4*a*c)
-    x1 = ((b* -1) - math.sqrt(delta)) /( 2 * a)
-    x2 = ((b* -1) + math.sqrt(delta)) /(2 * a)
-    print("x1 as first root: ", x1)
-    print("x2 as secound root: ", x2)
+    D = (b**2) - (4*a*c)
+    x1 = ((b* -1) - math.sqrt(D)) /( 2 * a)
+    x2 = ((b* -1) + math.sqrt(D)) /(2 * a)
+    print("the root x1:  ", x1)
+    print("the root x2:  ", x2)
 findRoot(a,b,c)
