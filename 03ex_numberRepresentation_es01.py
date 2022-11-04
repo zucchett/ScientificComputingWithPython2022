5899849c9e21bada63c4264e39d4d1e4f93aef23
AtiyeAbedinnia <115884751+AtiyeAbedinnia@users.noreply.github.com>
2022-11-04 18:54:25 +0100

    Update 03ex_numberRepresentation_es01.py

--- 03ex_numberRepresentation_es01.py
+++ 03ex_numberRepresentation_es01.py
@@ -17,18 +17,17 @@ def int_to_char(i):
  this function takes an input string, reads it according to a base representation
   and finally returns the number
 '''
-def read_base_sng(stri, base):
+def read_num(stri, base):
     number = 0
     stri_len = len(stri) - 1
     for i in range(0, stri_len +1):
-        # print(' reading', i, stri[i], char_to_int(stri[i]))
         number += char_to_int(stri[i]) * (base**(stri_len-i))
     return number
 '''
- this functions converts a number to the corresponding string representation
+ this functions converters a number to the corresponding representation
  in a given base 
 '''
-def num_to_representation(number, base):
+def change_to_base_rep(number, base):
     string = ''
     while number:
         string = int_to_char(number%base) + string
@@ -37,32 +36,51 @@ def num_to_representation(number, base):
 '''
 conversion amongs bin, dec, hex
 '''
-def convert(s, target):
+def converter(s, format_n):
+    
     s = s.replace(" ", "")  # removing all spaces in the sng
     s = s.lower()           # using only lowercased letters
-    if s[0] == '-': sign_isnegative = True
-    else: sign_isnegative = False
-    s = s.replace("-", "");   s = s.replace("+", "");
+    
+    if s[0] == '-': 
+        sign = -1
+    else: 
+        sign = 1
+        
+    s = s.replace("-", "")
+    s = s.replace("+", "")
     
     if s.startswith('0b'): # the input sng represents a binary num
-        num = (1 if s[0] == '0' else -1) * read_base_sng(s[2:],2)
+        if s[0] == '0':
+            num = read_num(s[2:], 2)
+        else:
+            num = -1 * read_num(s[2:], 2)
+    
     elif s.startswith('0x'): # the input sng represents a hex num
-        num = (1 if s[0] == '0' else -1) * read_base_sng(s[2:],16)
-    else: # the input sng must be decimal
-        num = read_base_sng(s,10)
+        if s[0] == '0':
+            num = read_num(s[2:], 16)
+        else:
+            num = -1 * read_num(s[2:], 16)
+    else:   
+        num = read_num(s, 10)
 
-    # print(' num in memory:', num)
-    
-    if target == 'dec':      out = num_to_representation(abs(num), 10)
-    elif target == 'bin':    out = '0b' + num_to_representation(abs(num), 2)
-    elif target == 'hex':    out = '0x' + num_to_representation(abs(num), 16)
+        
+            
+    if format_n == 'dec':      
+        result = change_to_base_rep(abs(num), 10)
+    elif format_n == 'bin':    
+        result = '0b' + change_to_base_rep(abs(num), 2)
+    elif format_n == 'hex':    
+        result = '0x' + change_to_base_rep(abs(num), 16)
     else:
-        print('invalid target format')
+        print('invalid format_n')
         return 0
+    if sign == -1:
+        return  '-' + result
+    else:
+        return result
     
-    return ('-' if sign_isnegative else '') + out
-    
-print('0b111 to dec :', convert('0b111','dec'))      # expected  7
-print('124 to hex:', convert('124','hex'))        # expected  0x7c
 
 
+print('0b111 to dec :', converter('-0b111','dec'))      # expected  7
+print('124 to hex:', converter('124','hex'))            # expected  0x7c
+print('0x7c to bin:', converter('0x7c','bin'))          # expected  0b1111100
