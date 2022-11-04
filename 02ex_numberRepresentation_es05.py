1ca77a217f81e8172e1336b954ba52f6d586eb52
AtiyeAbedinnia <115884751+AtiyeAbedinnia@users.noreply.github.com>
2022-11-04 11:54:48 +0100

    Update 02ex_numberRepresentation_es05.py

--- 02ex_numberRepresentation_es05.py
+++ 02ex_numberRepresentation_es05.py
@@ -2,4 +2,4 @@ language_scores = [('Python', 97), ('Cplusplus', 81), ('Php', 45), ('Java', 32)]
 
 
 language_scores.sort(key = lambda x : x[1])
-print(language_scores)
+print('sorted tuple (increasing order): ', language_scores)
