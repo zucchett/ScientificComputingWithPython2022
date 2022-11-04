36e757f52069095dba5a9a78bf59dbbc9cfd2688
AtiyeAbedinnia <115884751+AtiyeAbedinnia@users.noreply.github.com>
2022-11-04 11:42:31 +0100

    Update 02ex_numberRepresentation_es02.py

--- 02ex_numberRepresentation_es02.py
+++ 02ex_numberRepresentation_es02.py
@@ -1,2 +1,2 @@
-print([ x*x for x in range(5) if x%2 == 0 ])
-
+#ans = list(map(lambda x: x * x, filter(lambda x: x % 2 == 1, range(10)))
+print('ans by using list comprehension: ', [ x*x for x in range(5) if x%2 == 0 ])
