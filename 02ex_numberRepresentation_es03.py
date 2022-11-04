c140c710162fb12fef65807366ef96f51c3e128b
AtiyeAbedinnia <115884751+AtiyeAbedinnia@users.noreply.github.com>
2022-11-04 11:45:23 +0100

    Update 02ex_numberRepresentation_es03.py

--- 02ex_numberRepresentation_es03.py
+++ 02ex_numberRepresentation_es03.py
@@ -4,4 +4,5 @@ def Myfunc(words_list, n):
 
 n = 5
 words_list = ['red', 'blue', 'yellow', 'violet', 'green']
-print(Myfunc(words_list, n))
+print('list :', words_list)
+print('list of words that are shorter than n=5', Myfunc(words_list, n))
