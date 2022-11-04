e943e08756cada2c0713512aba131072e481fa02
AtiyeAbedinnia <115884751+AtiyeAbedinnia@users.noreply.github.com>
2022-11-04 11:33:40 +0100

    Update 02ex_numberRepresentation_es04.py

--- 02ex_numberRepresentation_es04.py
+++ 02ex_numberRepresentation_es04.py
@@ -1,9 +1,8 @@
 lang = {"Python" : 3, "Java" : '', "Cplusplus" : 'test', "Php" : 0.7}
 
 def func(lang):
-    x = list(lang.keys())
-    y = []
-    for ii in x:
-        y.append(len(ii))
-    return y  
-func(lang)
+   
+    return list(map(lambda keys:len(keys), list(lang.keys())))
+
+                
+print('Length of the keys of the lang dictionary: ', func(lang))
