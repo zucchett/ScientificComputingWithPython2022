696c33d331cf4b18c9f05ec589e72afc53396909
AtiyeAbedinnia <115884751+AtiyeAbedinnia@users.noreply.github.com>
2022-11-04 11:35:53 +0100

    Update 02ex_numberRepresentation_es09.py

--- 02ex_numberRepresentation_es09.py
+++ 02ex_numberRepresentation_es09.py
@@ -1,11 +1,21 @@
+'''
+Fibonacci sequence using for loop
+'''
+import timeit
 fib= [0, 1]
 def loopFibonacci(n):
     for i in range(2, n):
         fib.append(fib[i-1]+fib[i-2])
         
         
-'''
-%timeit 3.25 µs ± 305 ns per loop (mean ± std. dev. of 7 runs, 100,000 loops each)
 
-using for loop is more time consuming
+start = timeit.default_timer()
+loopFibonacci(20)
+exe_time = timeit.default_timer() - start
+print("Fibonacci sequence using for loop execution time  :", exe_time)
+
+
 '''
+recurcive function is way more time consuming than a for loop. the reason is by calling recursive function,  function should be called n-1 and n-2 times inside the function; in total 2n-3 operation. But in the for loop it goes from 0 to n-1. 
+'''
+
