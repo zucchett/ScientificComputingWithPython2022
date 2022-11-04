4ca6b7609510aa52e22e094fffba086896457f77
Vannesa Cifuentes <116501277+svcifuentes@users.noreply.github.com>
2022-11-04 11:25:08 +0100

    Add files via upload

---
+++ 01ex_introduction_es02.py
@@ -0,0 +1,15 @@
+#EXCERSICE 2
+
+
+print("-------Exercise #2: The swap ------")
+
+a = input('Enter value of a: ')
+b = input('Enter value of b: ')
+
+
+temp = a
+a = b
+b = temp
+
+print('The value of a after swapping: {}'.format(a))
+print('The value of b after swapping: {}'.format(b))
