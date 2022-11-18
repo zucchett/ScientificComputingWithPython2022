78ce3d055b7f6625ece0e1eb569208219fb0ee6c
alexandru_timis <alexandru.timis@studenti.unipd.it>
2022-11-18 16:48:58 +0100

    Problems with revert

--- 04exer/04ex_numpy_es03.py
+++
@@ -1,8 +0,0 @@
-#Alexandru Timis, 2039336, alexandru.timis@studenti.unipd.it
-import numpy as np
-import numpy.random as npr
-
-b = npr.uniform(0., 3.,(10,6))
-print("The original matrix is: \n",b)
-b[b<0.3]= 0.
-print("The modified matrix is: \n",b)
