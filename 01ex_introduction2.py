7eb62ce084ee64be118bc9070f36d77bd16b1360
Ronaldo Nazo <Ronaldo.nazo@raiffeisen.al>
2022-12-02 22:04:12 +0100

    HW 4-7

--- 01ex_introduction2.py
+++
@@ -1,9 +0,0 @@
-x = str(input("Enter First variable x: "))
-y = str(input("Enter Second variable y: "))
-x = x + "/n" + y
-y = x.split("/n")[0]
-x = x.split("/n")[1]
-
-print("First Value that you entered is: "+ x)
-print("Second Value that you entered is: "+ y)
-# print(type(x))
--- 02ex_fundamentals4.py
+++
@@ -1,9 +0,0 @@
-# 4. Map dictionary
-# Consider the following dictionary:
-
-lang = {"Python" : 3, "Java" : '', "Cplusplus" : 'test', "Php" : 0.7}
-
-# Write a function that takes the above dictionary and uses the map() higher order function
-#  to return a list that contains the length of the keys of the dictionary.
-lang_map = list(map(lambda x: len(x) ,lang.keys()))
-print(lang_map)
