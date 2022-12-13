4140cfcb4abd8dcee707163e21e5528f0a1e8b1c
abdallayaz <115219674+abdallayaz@users.noreply.github.com>
2022-12-13 17:27:26 +0100

    Delete 02_fundemental directory

--- 02_fundemental/02ex_fundamentals_es05.py
+++
@@ -1,3 +0,0 @@
-language_scores = [('Python', 97), ('Cplusplus', 81), ('Php', 45), ('Java', 32)]
-language_scores.sort(key = lambda i:i[0])
-language_scores
--- 02_fundemental/02x_fundamentals_es03.py
+++
@@ -1,7 +0,0 @@
-def shorter_words(x , n):
-    new_list = list(filter(lambda v: len(v)<n, x))
-    return new_list
-
-x = ['Mofmfglmpfg','Ha','R','D','ll','tkmtrmgitrmt']
-
-shorter_words(x , 3)
